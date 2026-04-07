9606869522958acd3720ea28e71ba9915f013c96
diya <shresthadiya59@gmail.com>
2026-04-07 09:12:53 +0545

    custom user, backup data

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'user',
     
     "rest_framework",
     'rest_framework.authtoken',
@@ -128,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+AUTH_USER_MODEL = 'user.User'
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
